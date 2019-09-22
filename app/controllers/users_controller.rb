1d04dd251e65e77c0efe05d2270a0a11f05b2ca9
kons16 <ks1216r@gmail.com>
2019-09-22 12:26:20 +0900

    空白のキーワードを登録させない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,15 +21,17 @@ class UsersController < ApplicationController
   # 趣味を新たに追加する
   def add_hoby
     @add_hoby = params[:hoby_name]
-    # 同じ名前の趣味がまだ登録されていないときのみ登録
-    my_hobies = current_user.hoby.pluck(:hoby_name)
-    if my_hobies.exclude?(@add_hoby)
-      current_user.user_hoby.build(hoby: Hoby.find_or_create_by(hoby_name: @add_hoby))
-      if current_user.save
+    if @add_hoby != ""
+      # 同じ名前の趣味がまだ登録されていないときのみ登録
+      my_hobies = current_user.hoby.pluck(:hoby_name)
+      if my_hobies.exclude?(@add_hoby)
+        current_user.user_hoby.build(hoby: Hoby.find_or_create_by(hoby_name: @add_hoby))
+        if current_user.save
+          redirect_to user_path(current_user.search_id)
+        end
+      else
         redirect_to user_path(current_user.search_id)
       end
-    else
-      redirect_to user_path(current_user.search_id)
     end
   end
 
