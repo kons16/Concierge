02a38d66815a79a10ca63d6403f33dfba840e138
kons16 <ks1216r@gmail.com>
2019-09-29 14:50:39 +0900

    s3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: '',
-      aws_secret_access_key: '',
+      aws_access_key_id: "<%= ENV['IAM_ACCESS_KEY'] %>",
+      aws_secret_access_key: "<%= ENV['IAM_SECRET_KEY'] %>",
       region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'rails-photo-123'
+  config.fog_directory  = 'chatapp-myicon'
   config.cache_storage = :fog
 end
