4d099b0ae3975ef649c602259dac30ad885e7817
kons16 <ks1216r@gmail.com>
2019-10-11 09:07:56 +0900

    This commit fix #3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,22 @@
 //= require_tree .
 //= require bootstrap-sprockets
 // = require data-confirm-modal
+
+window.addEventListener('load', main, false);
+function main(e) {
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    
+    function sleep(waitMsec) {
+        var startMsec = new Date();
+        while (new Date() - startMsec < waitMsec);
+    }
+
+    function jsLoaded() {
+        // 読み込みが終了したとき「入力中です」アナウンスが表示されていたら、1秒後に非表示にする。
+        var announce = document.getElementById('on_input_status').innerHTML;
+        if(announce == "相手ユーザーが入力中です"){
+            sleep(1000); // 1秒 == 1000
+            document.getElementById('on_input_status').innerHTML = "";
+        }
+    }
+}
