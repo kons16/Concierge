6905b20db8d66b897c984e70e5f80a18d7141d65
kons16 <ks1216r@gmail.com>
2020-04-17 13:34:41 +0900

    hobyにdependent: destroyを追加

--- app/models/hoby.rb
+++ app/models/hoby.rb
@@ -1,5 +1,5 @@
 class Hoby < ApplicationRecord
-  has_many :user_hoby
+  has_many :user_hoby, dependent: :destroy
   has_many :user, through: :user_hoby
   accepts_nested_attributes_for :user_hoby
 end
